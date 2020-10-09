3ed77928d56bec7110ca0193a6d84e649f86ca88
Omar Assaad <omar.ali.assaad@gmail.com>
2020-10-09 16:20:48 -0600

    updated MUX2TO1

--- tb_MUX2TO1.vhd
+++ tb_MUX2TO1.vhd
@@ -37,19 +37,19 @@ ARCHITECTURE behavior OF tb_MUX2TO1 IS
       stim_process: process -- this process, in testbench/simulation code, is different than in design code
       begin
 		  assert false report "MUX2TO1 testbench started"; -- puts a note in the ModelSim transcript window (this line is just for convenience)
-		  wait for time_delay;
+		  
 		  in1 <= "0000000000000000"; in2 <= "0000000000000000"; 
-        wait for time_delay;
 		  s <= '0'; 
-		  wait for time_delay;
+		  wait for time_delay*1000;
+		  
 		  in1 <= "0101010101010101"; in2 <= "1010101010101010"; 
-        wait for time_delay;
+		  wait for time_delay*1000;
 		  in1 <= "1010101010101010"; in2 <= "0101010101010101"; 
-        wait for time_delay;
+          wait for time_delay*1000;
 		  s <= '1'; 
-		  wait for time_delay;
+		  wait for time_delay*1000;
 		  in1 <= "0101010101010101"; in2 <= "1010101010101010"; 
-        wait for time_delay;
+          wait for time_delay*1000;
 		  s <= '0'; 
 		  wait for 10*time_delay; -- this extends the time by 10x the time_delay, for ease of veiwing waveforms
 		  assert false report "MUX2TO1 testbench completed"; -- puts a note in the ModelSim transcript window (this line is just for convenience)
